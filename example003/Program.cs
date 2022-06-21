60a54c0dd2a91f3d30b708160db5860a40a7041f
SfShev <pshev1406@gmail.com>
2022-06-21 23:14:42 +0300

    add random

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int nuberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1,2,3....9
+Console.WriteLine(numberA) ;
+int nuberB = new Random().Next(1, 10); // 1,2,3....9
+Console.WriteLine(nuberB) ;
 int result = numberA + nuberB;
 Console.WriteLine(result) ;
