47ba707f118da5102c5f40449b7adb6e96689c97
SfShev <pshev1406@gmail.com>
2022-06-25 19:06:48 +0300

    четное нечетное

---
+++ example013/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Если хотите узнать четное это число или нет");
+Console.WriteLine("Введите число");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 %2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
