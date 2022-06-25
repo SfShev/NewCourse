52b94d14f8b329649775cbe1546dd7efafd6fccb
SfShev <pshev1406@gmail.com>
2022-06-25 18:50:33 +0300

    Максимальное из 3 чисел

---
+++ example012/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Если хотите узнать какое число больше из трех чисел");
+Console.WriteLine("Введите 3 числа");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (number1 > max)
+    max = number1;
+
+if (number2 > max)
+    max = number2;
+else max = number3;
+
+Console.WriteLine("Число: " + max + " больше остальных");
