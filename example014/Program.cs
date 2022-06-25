5996cf00ef875b527fb1fcfa670e4b83e9b57298
SfShev <pshev1406@gmail.com>
2022-06-25 19:30:50 +0300

    Четные числа от 1 до N

---
+++ example014/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Если хотите узнать все четные числа от 1 до выбранного числа");
+Console.WriteLine("Введите число");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+
+while (index <= number1)
+{
+    Console.Write($"{index} ");
+
+
+    index += 2;
+}
