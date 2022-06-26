b082a942b4c39025d317cb8c928f7808f67bae34
SfShev <pshev1406@gmail.com>
2022-06-26 18:20:07 +0300

    вторая цифра трехзначного числа

---
+++ example016/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Если хотите узнать какая вторая цифра этого трехзначного числа");
+Console.WriteLine("Введите трехзначное число");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int a = number1 % 100;
+int b = a / 10;
+
+
+Console.WriteLine(b);
+
