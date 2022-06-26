3fc01ccc0e87168ce76291e0778aaf3c85dbe286
SfShev <pshev1406@gmail.com>
2022-06-26 19:29:48 +0300

    выходной или будни по номеру дня

---
+++ example018/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите номер дня недели");
+
+int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Номер дня: " + day );
+
+switch (day)
+{
+    case 1: Console.WriteLine("Нет, будний день"); break;
+    case 2: Console.WriteLine("Нет, будний день"); break;
+    case 3: Console.WriteLine("Нет, будний день"); break;
+    case 4: Console.WriteLine("Нет, будний день"); break;
+    case 5: Console.WriteLine("Нет, будний день"); break;
+    case 6: Console.WriteLine("Да,выходной"); break;
+    case 7: Console.WriteLine("Да,выходной"); break;
+    default: Console.WriteLine("Неверный ввод"); break;
+}
