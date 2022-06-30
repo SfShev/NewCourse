f90465a826e46222baecea01285fd8ce6df3a5e4
SfShev <Pshev1406@gmail.com>
2022-06-30 13:01:17 +0300

    Лекция 2 матоды и массивы

---
+++ example019_arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿void Fillarray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write($"{col[pos]} ");
+        pos++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+Fillarray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
