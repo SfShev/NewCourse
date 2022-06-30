1b97e17af2cc32e8d2910e271086b1879b70ff67
SfShev <Pshev1406@gmail.com>
2022-06-30 12:34:02 +0300

    Добавил метод с массивом

--- example015_methods_and_arrays/Program.cs
+++ example015_methods_and_arrays/Program.cs
@@ -23,3 +23,25 @@ int max = Max(Max(a, b, c),
           Max(a2, b2, c2));
 
 Console.WriteLine(max);
+
+
+
+int Max1(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+
+    if (result < arg2) result = arg2;
+    if (result < arg3) result = arg3;
+    return result;
+}
+
+int[] array = { 156, 22, 3993, 47, 56, 64, 37, 82, 29 };
+
+
+
+int max1 = Max1(
+    Max1(array[0], array[1], array[2]),
+    Max1(array[3], array[4], array[5]),
+    Max1(array[6], array[7], array[8]));
+
+Console.WriteLine(max1);
