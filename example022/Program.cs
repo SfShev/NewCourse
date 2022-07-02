7cd102e9ef7cc2cc9e7768bc2a2cc05744fcdac0
SfShev <pshev1406@gmail.com>
2022-07-02 12:00:42 +0300

    Квадраты чисел от 0 до N

---
+++ example022/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+
+
+if (number1 > 0)
+
+    while (index <= number1)
+    {
+        Console.Write($"{index * index} ");
+        index++;
+    }
+else
+    while (index >= number1)
+    {
+        Console.Write($"{index * index} ");
+        index -= 1;
+    }
