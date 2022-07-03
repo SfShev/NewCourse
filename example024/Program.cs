f9fc464e21ec7a7f9ceabcc51e4da2e03ccbe8a4
SfShev <Pshev1406@gmail.com>
2022-07-03 16:02:11 +0300

    Произведение от 1 до N

---
+++ example024/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int ProductNumber(int num)
+{
+    int product = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        product *= i;
+    }
+    return product;
+}
+Console.WriteLine(ProductNumber(num));
