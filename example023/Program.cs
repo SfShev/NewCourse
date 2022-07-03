9e1ce84f20a6eea5aa690eab9204d3aa68fa50e0
SfShev <Pshev1406@gmail.com>
2022-07-03 16:01:22 +0300

    сумма чисел от одного до n

---
+++ example023/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetSum(int a)
+{
+    int sum = 0;
+
+    for (int i = 0; i <= a; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+Console.WriteLine("введите число");
+int num = int.Parse (Console.ReadLine()!);
+Console.WriteLine (GetSum(num));
