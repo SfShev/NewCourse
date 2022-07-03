3a074cb597ffcc89f982ef43ad193001aabcc8e2
SfShev <Pshev1406@gmail.com>
2022-07-03 18:35:41 +0300

    Возведение числа в степень

---
+++ example025/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int Exponentiation(int RaisedNumber, int Exponent)
+{
+    int exponentiation = 1;
+    for (int i = 0; i < Exponent; i++)
+    {
+        exponentiation *= RaisedNumber;
+    }
+    return exponentiation;
+}
+Console.WriteLine("Введите число которое хотите возвести в степень: ");
+int raised_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень хотите возвети? ");
+int exponent = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("{0} ^ {1} = {2}", raised_number, exponent, Exponentiation(raised_number, exponent));
