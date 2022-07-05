08af045b0fc8ebfc8600ced81320bbd704b47006
SfShev <Pshev1406@gmail.com>
2022-07-05 14:51:01 +0300

    Ввод массива с клавиатуры

---
+++ example027/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] Collection (int elements)
+{
+int[] collection = new int[elements];
+Console.WriteLine("Заполните массив: ");
+
+for (int i = 0; i < collection.Length; i++)
+{
+    Console.WriteLine();
+    Console.Write($"Введите элемент массива {i}: "  );
+    collection[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Ваш массив: ");
+
+for (int i = 0; i < collection.Length; i++)
+{
+    Console.Write($"{collection[i]},");
+}
+return collection;
+}
+
+Console.Write("Хотите создать массив? Введите длину массива: ");
+int arraysize = Convert.ToInt32(Console.ReadLine()); 
+
+Collection(arraysize);
