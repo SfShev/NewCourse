0ccea1db2598cb27377b906993bb8d054e1cfdf1
SfShev <pshev1406@gmail.com>
2022-07-08 13:11:03 +0300

    Ввод массива 3 вариант(самый правильный)

---
+++ example029/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Хотите создать массив? Введите длину массива: ");
+int arraysize = Convert.ToInt32(Console.ReadLine());
+int[] collection = new int[arraysize];
+Console.WriteLine("Заполните массив: ");
+
+
+int[] Collection(int elements)
+{
+    for (int i = 0; i < elements; i++)
+    {
+        Console.WriteLine();
+        Console.Write($"Введите элемент массива # {i + 1}: ");
+        collection[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return collection;
+}
+
+
+void PrintCollection()
+{
+    Console.WriteLine();
+    Console.WriteLine("Ваш массив: ");
+
+    foreach (var item in collection)
+    {
+        Console.Write(item + ",");
+    }
+
+}
+
+Collection(arraysize);
+PrintCollection();
+System.Console.WriteLine("\b \b");
