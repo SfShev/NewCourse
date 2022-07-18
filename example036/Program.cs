bb9a91a6113e90c7e8dde427050bde24b27f9ad1
SfShev <Pshev1406@gmail.com>
2022-07-18 17:47:10 +0300

    Заполнение двухмерного массива рандомными вещественными числами

---
+++ example036/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите параметры матрицы");
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите диапозон чисел матрицы");
+Console.Write("Введите минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+double[,] GetMatrix(int rowsMtrx, int columsMtrx, int minNum, int maxNum)
+{
+    double[,] matrix = new double[rows, colums];
+    var random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1) + random.NextDouble();
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            Console.Write($" {Math.Round(matrix[i, j], 1),4}|");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] mtrx = GetMatrix(rows,colums,min,max);
+ShowMatrix(mtrx);
