8db383f18156db0a96fc43100022a7f2e8edca71
SfShev <Pshev1406@gmail.com>
2022-07-19 15:07:14 +0300

    Сколько раз встречается число [0,9]

---
+++ example041/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("Введите параметры матрицы");
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите диапозон чисел матрицы");
+Console.Write("Введите минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+
+
+int[,] GetMatrix(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    var random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            Console.Write($" {matrix[i, j],1}|");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FrequencyDictionary(int[,] matrix)
+{
+    int[] array = new int[10];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[matrix[i, j]]++;
+        }
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Число {i} встречалось {array[i]} раз");
+    }
+}
+
+
+int[,] mtrx = GetMatrix(rows, colums, min, max);
+
+ShowMatrix(mtrx);
+Console.WriteLine();
+FrequencyDictionary(mtrx);
