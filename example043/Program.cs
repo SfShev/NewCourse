766e5aae73cc0757303ebe30f709b4fd99359995
SfShev <Pshev1406@gmail.com>
2022-07-19 20:23:55 +0300

    1111

---
+++ example043/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.WriteLine("Введите параметры матрицы");
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите диапозон чисел матрицы");
+Console.Write("Введите минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] GetMatrix(int rowsMtrx, int columsMtrx, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    var random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            Console.Write($" {matrix[i, j],1}|");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] AscendingOrder(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < matrix[i, j++])
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[i, j++];
+                matrix[i, j++] = temp;
+            }
+        }
+    }
+    return matrix;
+}
+
+
+
+
+int[,] mtrx = GetMatrix(rows, colums, min, max);
+ShowMatrix(mtrx);
+Console.WriteLine();
+int[,] matr = AscendingOrder(mtrx);
+ShowMatrix(matr);
