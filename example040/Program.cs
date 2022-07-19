d8d7ab07a5b4706c73e579fedada2629ec4d9b62
SfShev <Pshev1406@gmail.com>
2022-07-19 14:29:12 +0300

    Транспонирование квадратной матрицы

---
+++ example040/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("Введите параметры матрицы");
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите диапозон чисел матрицы");
+Console.Write("Введите минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+
+
+int[,] GetMatrix(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    var random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            Console.Write($" {matrix[i, j],1}|");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SwapRowsAndColumsMatrix(int[,] matrix)
+{
+    int[,] matr = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = matrix[j,i];
+        }
+    }
+    return matr;
+}
+
+
+int[,] mtrx = GetMatrix(rows, colums, min, max);
+ShowMatrix(mtrx);
+Console.WriteLine();
+ShowMatrix(SwapRowsAndColumsMatrix(mtrx));
