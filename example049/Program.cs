4d9ce0ce88915b0bb28c891ce8c63de2ed5e313a
SfShev <pshev1406@gmail.com>
2022-07-24 20:52:31 +0300

    Функция аккермана

---
+++ example049/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Функция Аккермана");
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int AckermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return AckermanFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    }
+
+}
+
+void PrintAckerman()
+{
+    Console.Write($"A({m},{n}) = {AckermanFunction(m, n)}");
+}
+
+PrintAckerman();
